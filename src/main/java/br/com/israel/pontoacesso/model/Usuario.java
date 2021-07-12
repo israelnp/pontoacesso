57187b76923f2d322107b17f65328ad938b6f8b8
israelnp <israelpereira1984@gmail.com>
2021-07-12 20:19:17 -0300

    Mapear modelo Usuario

--- src/main/java/br/com/israel/pontoacesso/model/Usuario.java
+++ src/main/java/br/com/israel/pontoacesso/model/Usuario.java
@@ -2,6 +2,9 @@ package br.com.israel.pontoacesso.model;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -12,13 +15,19 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @EqualsAndHashCode
 @Builder
+@Entity
 public class Usuario {
 
+    @Id
     private Long id;
+    @ManyToOne
     private CategoriaUsuario categoriaUsuario;
     private String nome;
+    @ManyToOne
     private Empresa empresa;
+    @ManyToOne
     private NivelAcesso nivelAcesso;
+    @ManyToOne
     private  JornadaTrabalho jornadaTrabalho;
     private BigDecimal tolerancia;
     private LocalDateTime inicioJornada;
