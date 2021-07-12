376c4accfb0569f69693789acaa0024c5daafe02
israelnp <israelpereira1984@gmail.com>
2021-07-12 18:15:50 -0300

    Criar configuracao inicial do Spring Boot

---
+++ src/main/java/br/com/israel/pontoacesso/PontoacessoApplication.java
@@ -0,0 +1,13 @@
+package br.com.israel.pontoacesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoacessoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PontoacessoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/israel/pontoacesso/PontoacessoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.israel.pontoacesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoacessoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
