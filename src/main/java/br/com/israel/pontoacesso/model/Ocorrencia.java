bb1a1f8038aeb9dbc0eb6d1176650836bc046c76
israelnp <israelpereira1984@gmail.com>
2021-07-12 18:37:49 -0300

    Criar modelo de negocio Ocorrencia

---
+++ src/main/java/br/com/israel/pontoacesso/model/Ocorrencia.java
@@ -0,0 +1,16 @@
+package br.com.israel.pontoacesso.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Ocorrencia {
+
+    private long id;
+    private String nome;
+    private String descricao;
+}
