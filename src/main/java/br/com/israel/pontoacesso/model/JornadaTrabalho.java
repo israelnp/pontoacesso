2c65c2d4ec8fa3e6e361d742f7932a689fa8b034
israelnp <israelpereira1984@gmail.com>
2021-07-12 19:59:52 -0300

    Mapear modelo JornadaTrabalho

--- src/main/java/br/com/israel/pontoacesso/model/JornadaTrabalho.java
+++ src/main/java/br/com/israel/pontoacesso/model/JornadaTrabalho.java
@@ -2,14 +2,21 @@ package br.com.israel.pontoacesso.model;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
 @Builder
+@Entity
 public class JornadaTrabalho {
 
+    @Id
+    @GeneratedValue
     private long id;
     private String descricao;
 }
