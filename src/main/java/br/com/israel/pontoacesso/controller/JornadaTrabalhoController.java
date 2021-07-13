4dce3a4b2d3ec1c501fba8a5d4cdc39f8ad6d9e5
israelnp <israelpereira1984@gmail.com>
2021-07-12 22:59:20 -0300

    Criar endpoint para salvar uma jornada de trabalho

--- src/main/java/br/com/israel/pontoacesso/controller/JornadaTrabalhoController.java
+++ src/main/java/br/com/israel/pontoacesso/controller/JornadaTrabalhoController.java
@@ -1,7 +1,26 @@
 package br.com.israel.pontoacesso.controller;
 
+import br.com.israel.pontoacesso.model.JornadaTrabalho;
+import br.com.israel.pontoacesso.service.JornadaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/jornada")
 public class JornadaTrabalhoController {
+
+    JornadaService jornadaService;
+
+    @Autowired
+    public JornadaTrabalhoController(JornadaService jornadaService) {
+        this.jornadaService = jornadaService;
+    }
+
+    @PostMapping
+    public JornadaTrabalho salvar(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return this.jornadaService.salvarJornada(jornadaTrabalho);
+    }
 }
