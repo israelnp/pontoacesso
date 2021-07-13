17f094e76f4e4c6dd0d881937d544da97e025ade
israelnp <israelpereira1984@gmail.com>
2021-07-12 23:11:30 -0300

    Criar servico para buscar todas as jornadas de trabalho

--- src/main/java/br/com/israel/pontoacesso/service/JornadaService.java
+++ src/main/java/br/com/israel/pontoacesso/service/JornadaService.java
@@ -4,6 +4,8 @@ import br.com.israel.pontoacesso.model.JornadaTrabalho;
 import br.com.israel.pontoacesso.repository.JornadaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 public class JornadaService {
 
     JornadaRepository jornadaRepository;
@@ -15,6 +17,10 @@ public class JornadaService {
 
     public JornadaTrabalho salvarJornada(JornadaTrabalho jornadaTrabalho){
         return this.jornadaRepository.save(jornadaTrabalho);
+    }
+
+    public List<JornadaTrabalho> buscarTodasJornadas(){
+        return this.jornadaRepository.findAll();
     }
 
 
