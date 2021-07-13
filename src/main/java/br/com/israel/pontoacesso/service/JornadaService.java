45900e1f4a3087859a7ce3a6f79ca3f731f823d9
israelnp <israelpereira1984@gmail.com>
2021-07-13 01:45:48 -0300

    Adicionar Anotation de servico

--- src/main/java/br/com/israel/pontoacesso/service/JornadaService.java
+++ src/main/java/br/com/israel/pontoacesso/service/JornadaService.java
@@ -3,10 +3,12 @@ package br.com.israel.pontoacesso.service;
 import br.com.israel.pontoacesso.model.JornadaTrabalho;
 import br.com.israel.pontoacesso.repository.JornadaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class JornadaService {
 
     JornadaRepository jornadaRepository;
