95198a59b00abd6d97f5243962ae556e7eae6101
israelnp <israelpereira1984@gmail.com>
2021-07-13 16:56:44 -0300

    Adaptar os endpoints a nova realidade de comunicacao da api com exterior

--- src/main/java/br/com/israel/pontoacesso/api/controller/JornadaTrabalhoController.java
+++ src/main/java/br/com/israel/pontoacesso/api/controller/JornadaTrabalhoController.java
@@ -1,52 +1,65 @@
 package br.com.israel.pontoacesso.api.controller;
 
+import br.com.israel.pontoacesso.api.assembler.JornadaTrabalhoInputDisassembler;
+import br.com.israel.pontoacesso.api.assembler.JornadaTrabalhoAssembler;
+import br.com.israel.pontoacesso.api.model.JornadaTrabalhoModel;
 import br.com.israel.pontoacesso.domain.exception.JornadaTrabalhoNaoEncontradaException;
 import br.com.israel.pontoacesso.domain.exception.NegocioException;
 import br.com.israel.pontoacesso.domain.model.JornadaTrabalho;
+import br.com.israel.pontoacesso.api.model.input.JornadaTrabalhoInput;
 import br.com.israel.pontoacesso.domain.service.JornadaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/jornada")
+@RequestMapping(path = "/jornada", produces = MediaType.APPLICATION_JSON_VALUE)
 public class JornadaTrabalhoController {
 
     JornadaService jornadaService;
-
+    JornadaTrabalhoAssembler jornadaTrabalhoAssembler;
+    JornadaTrabalhoInputDisassembler jornadaTrabalhoInputDisassembler;
 
     @Autowired
-    public JornadaTrabalhoController(JornadaService jornadaService) {
+    public JornadaTrabalhoController(
+                JornadaService jornadaService,
+                JornadaTrabalhoAssembler jornadaTrabalhoAssembler,
+                JornadaTrabalhoInputDisassembler jornadaTrabalhoInputDisassembler
+            ) {
         this.jornadaService = jornadaService;
+        this.jornadaTrabalhoAssembler = jornadaTrabalhoAssembler;
+        this.jornadaTrabalhoInputDisassembler = jornadaTrabalhoInputDisassembler;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public JornadaTrabalho salvarJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
-        return this.jornadaService.salvarJornada(jornadaTrabalho);
+    public JornadaTrabalhoModel salvarJornada(@RequestBody @Valid JornadaTrabalhoInput jornadaTrabalhoInput){
+        return this.jornadaTrabalhoAssembler.toModel(this.jornadaService
+                .salvarJornada(this.jornadaTrabalhoInputDisassembler.toDomainObject(jornadaTrabalhoInput)));
     }
 
     @GetMapping
-    public List<JornadaTrabalho> buscarTodasJornadasDeTrabalho(){
-        return this.jornadaService.buscarTodasJornadas();
+    public List<JornadaTrabalhoModel> buscarTodasJornadasDeTrabalho(){
+        this.jornadaService.buscarTodasJornadas();
+        return this.jornadaTrabalhoAssembler.paraListDeModel(this.jornadaService.buscarTodasJornadas());
     }
 
     @GetMapping("/{jornadaId}")
-    public JornadaTrabalho buscarJornadaDeTrabalhoPorId(@PathVariable Long jornadaId){
-        return this.jornadaService.buscarJornadaPorId(jornadaId);
+    public JornadaTrabalhoModel buscarJornadaDeTrabalhoPorId(@PathVariable Long jornadaId){
+        return this.jornadaTrabalhoAssembler.toModel(this.jornadaService.buscarJornadaPorId(jornadaId));
     }
 
-    @PutMapping
-    public JornadaTrabalho modificarJornadaDeTrabalho(@RequestBody JornadaTrabalho jornadaTrabalho){
+    @PutMapping("/{jornadaId}")
+    public JornadaTrabalhoModel modificarJornadaDeTrabalho(@PathVariable Long jornadaId, @RequestBody @Valid JornadaTrabalhoInput jornadaTrabalhoInput){
         try {
-            JornadaTrabalho jornadaAtual = this.jornadaService.buscarJornadaPorId(jornadaTrabalho.getId());
-
-            jornadaAtual.setDescricao(jornadaTrabalho.getDescricao());
-
+            JornadaTrabalho jornadaAtual = this.jornadaService.buscarJornadaPorId(jornadaId);
+            jornadaAtual.setDescricao(jornadaTrabalhoInput.getDescricao());
             jornadaAtual = this.jornadaService.salvarJornada(jornadaAtual);
-
-            return jornadaAtual;
+            return this.jornadaTrabalhoAssembler.toModel(jornadaAtual);
         } catch (JornadaTrabalhoNaoEncontradaException e) {
             throw new NegocioException(e.getMessage(), e);
         }
