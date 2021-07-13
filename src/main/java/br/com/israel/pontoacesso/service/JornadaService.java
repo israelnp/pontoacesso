df8591b91b41a47ce7665f90931d02a2acba863d
israelnp <israelpereira1984@gmail.com>
2021-07-12 22:41:30 -0300

    Criar servico para salvar uma jornada de trabalho

--- src/main/java/br/com/israel/pontoacesso/service/JornadaService.java
+++ src/main/java/br/com/israel/pontoacesso/service/JornadaService.java
@@ -1,4 +1,21 @@
 package br.com.israel.pontoacesso.service;
 
+import br.com.israel.pontoacesso.model.JornadaTrabalho;
+import br.com.israel.pontoacesso.repository.JornadaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class JornadaService {
+
+    JornadaRepository jornadaRepository;
+
+    @Autowired
+    public JornadaService(JornadaRepository jornadaRepository) {
+        this.jornadaRepository = jornadaRepository;
+    }
+
+    public JornadaTrabalho salvar(JornadaTrabalho jornadaTrabalho){
+        return this.jornadaRepository.save(jornadaTrabalho);
+    }
+
+
 }
