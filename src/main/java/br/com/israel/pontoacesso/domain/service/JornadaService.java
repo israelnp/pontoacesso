9c6444f971a48bfa664e98d3d6f9542c90e0472c
israelnp <israelpereira1984@gmail.com>
2021-07-13 11:58:36 -0300

    Integrar modelos de exception em os servicos de Jornada de trabalho

--- src/main/java/br/com/israel/pontoacesso/domain/service/JornadaService.java
+++ src/main/java/br/com/israel/pontoacesso/domain/service/JornadaService.java
@@ -1,15 +1,22 @@
 package br.com.israel.pontoacesso.domain.service;
 
+import br.com.israel.pontoacesso.domain.exception.EntidadeEmUsoException;
+import br.com.israel.pontoacesso.domain.exception.JornadaTrabalhoNaoEncontradaException;
 import br.com.israel.pontoacesso.domain.model.JornadaTrabalho;
 import br.com.israel.pontoacesso.domain.repository.JornadaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class JornadaService {
+
+    private static final String MSG_JORNADA_EM_USO
+            = "Jornada de código %d não pode ser removida, pois está em uso";
 
     JornadaRepository jornadaRepository;
 
@@ -26,16 +33,24 @@ public class JornadaService {
         return this.jornadaRepository.findAll();
     }
 
-    public Optional<JornadaTrabalho> buscarJornadaPorId(Long jornadaId){
-        return this.jornadaRepository.findById(jornadaId);
-    }
-
-    public JornadaTrabalho modificarJornada(JornadaTrabalho jornadaTrabalho){
-        return this.jornadaRepository.save(jornadaTrabalho);
+    public JornadaTrabalho buscarJornadaPorId(Long jornadaId){
+        return this.jornadaRepository.findById(jornadaId)
+                .orElseThrow(() -> new JornadaTrabalhoNaoEncontradaException(jornadaId));
     }
 
     public void removerJornadaPorId(Long jornadaId){
-         this.jornadaRepository.deleteById(jornadaId);
+        try {
+            this.jornadaRepository.deleteById(jornadaId);
+            this.jornadaRepository.flush();
+
+        } catch (EmptyResultDataAccessException e) {
+            throw new JornadaTrabalhoNaoEncontradaException(jornadaId);
+
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(
+                    String.format(MSG_JORNADA_EM_USO, jornadaId));
+        }
+
     }
 
 }
